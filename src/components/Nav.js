f718435253a1aadd6de8c8906f5471e5f52f3ee9
Jeffe <j.garner200@gmail.com>
2020-06-25 17:39:22 -0400

    some styling, resume updated

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ import resume from "../resume/Resume.pdf";
 
 export default function Nav() {
 	return (
-		<nav className="navbar navbar-expand-md navbar-light ">
+		<nav className="navbar navbar-expand-md navbar-dark ">
 			<Link className="navbar-brand logo-wrapper" to="/">
 				<img src={logo} alt="" className="logo" />
 			</Link>
@@ -25,17 +25,39 @@ export default function Nav() {
 			<div className="collapse navbar-collapse" id="navbarsExample04">
 				<ul className="navbar-nav mr-auto">
 					<Link className="nav-item" to="/">
-						<li className="nav-link" data-toggle="collapse" data-target=".navbar-collapse.show">Home</li>
+						<li
+							className="nav-link"
+							data-toggle="collapse"
+							data-target=".navbar-collapse.show"
+						>
+							Home
+						</li>
 					</Link>
 
 					<Link className="nav-item" to="/projects">
-						<li className="nav-link" data-toggle="collapse" data-target=".navbar-collapse.show">Projects</li>
+						<li
+							className="nav-link"
+							data-toggle="collapse"
+							data-target=".navbar-collapse.show"
+						>
+							Projects
+						</li>
 					</Link>
 					<Link className="nav-item" to="/aboutme">
-						<li className="nav-link" data-toggle="collapse" data-target=".navbar-collapse.show">About Me</li>
+						<li
+							className="nav-link"
+							data-toggle="collapse"
+							data-target=".navbar-collapse.show"
+						>
+							About Me
+						</li>
 					</Link>
 
-					<li className="nav-item dropdown" data-toggle="collapse" data-target=".navbar-collapse.show">
+					<li
+						className="nav-item dropdown"
+						data-toggle="collapse"
+						data-target=".navbar-collapse.show"
+					>
 						<a
 							className="nav-link dropdown-toggle"
 							href="/"
@@ -47,10 +69,10 @@ export default function Nav() {
 							Resume
 						</a>
 						<div className="dropdown-menu" aria-labelledby="dropdown04">
-							<a className="dropdown-item" href={resume} >
+							<a className="dropdown-item" href={resume}>
 								See it
 							</a>
-							<a className="dropdown-item" href={resume} download  >
+							<a className="dropdown-item" href={resume} download>
 								Download it
 							</a>
 						</div>
@@ -66,8 +88,13 @@ export default function Nav() {
 						>
 							Blog
 						</a>
-						<div className="dropdown-menu" aria-labelledby="dropdown04" data-toggle="collapse" data-target=".navbar-collapse.show">
-							<Link className="dropdown-item" to="/blog/wiut" >
+						<div
+							className="dropdown-menu"
+							aria-labelledby="dropdown04"
+							data-toggle="collapse"
+							data-target=".navbar-collapse.show"
+						>
+							<Link className="dropdown-item" to="/blog/wiut">
 								What I'm up to
 							</Link>
 							<Link className="dropdown-item" to="/blog/codechallenges">
